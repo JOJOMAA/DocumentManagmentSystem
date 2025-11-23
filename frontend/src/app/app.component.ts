07c5e584ae44d0d2d26aab60d53b2da08f854dee
Johannes Reitterer <131317456+JOJOMAA@users.noreply.github.com>
2025-11-23 16:34:14 +0100

    Feat: Enhance UI with responsive design and theme toggle functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {PdfUploadComponent} from "./pdf-upload/pdf-upload.component";
-import {PdfListComponent} from "./pdf-list/pdf-list.component";
+import { Component, Renderer2} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { PdfUploadComponent } from "./pdf-upload/pdf-upload.component";
+import { PdfListComponent } from "./pdf-list/pdf-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PdfUploadComponent, PdfListComponent],
+  imports: [PdfUploadComponent, PdfListComponent, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PaperlessFrontend';
+  isDarkMode = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    const theme = this.isDarkMode ? 'dark' : 'light';
+    if (this.isDarkMode) {
+      this.renderer.setAttribute(document.body, 'data-theme', 'dark');
+    } else {
+      this.renderer.removeAttribute(document.body, 'data-theme');
+    }
+  }
 }
